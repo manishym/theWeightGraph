1ee485ffc2c8ca8d616dedc93497ebb089891ef7
Manish M Yathnalli <manish.ym@gmail.com>
2011-07-10 21:24:15 +0530

    Migrated user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #:validates :email   :presence => true,
+                      
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Invalid users" do
+    before(:each) do
+      @invalid1 = {:name => "", :email => "", :password => ""}
+      @invalid2 = {:name => "", :email => "someone", :password => "value", }
+    end
+    it "should not create a user without email" do
+      empty_user = User.create!(@invalid1)
+      empty_user.should_not be_valid
+    end
+  end
 end
