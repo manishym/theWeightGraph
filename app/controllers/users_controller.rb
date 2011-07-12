2f55b127dcc28d6b5213b90a8f49984b976647c3
Manish M Yathnalli <manish.ym@gmail.com>
2011-07-12 18:14:14 +0530

    Stopped dev on this, using devise to authenticate

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.find(1)
+  end
+
+  def show
+  
+  end
+
+  def edit
+  
+  end
+
+  def destroy
+  
+  end
+  
+  def index
+    
+  end
+  
+  def new
+    
+  end
+  def create
+    
+  end
+  
+  def new
+    
+  end
+  def update
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TheWeightGraph::Application.routes.draw do
+  resources :users
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
